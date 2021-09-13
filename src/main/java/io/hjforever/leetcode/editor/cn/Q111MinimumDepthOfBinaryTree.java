29e9cc1ec52266ec834fe36c9a23bae4f050d12a
hjforever <thjforever@hotmail.com>
2021-09-13 21:31:54 +0800

    leetcode question 111 solution
    二叉树

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q111MinimumDepthOfBinaryTree.java
@@ -0,0 +1,87 @@
+//给定一个二叉树，找出其最小深度。
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明：叶子节点是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的范围在 [0, 10⁵] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 584 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+import io.hjforever.leetcode.editor.cn.common.TreeNode;
+
+public class Q111MinimumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new Q111MinimumDepthOfBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        /**
+         * 二叉树遍历，比较左右节点深度取最小
+         */
+        public int minDepth(TreeNode root) {
+            return depth(root);
+        }
+
+        private int depth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            if (root.left == null && root.right == null) {
+                return 1;
+            }
+            int minDepth = Integer.MAX_VALUE;
+            if (root.left != null) {
+                minDepth = Math.min(minDepth, depth(root.left));
+            }
+            if (root.right != null) {
+                minDepth = Math.min(minDepth, depth(root.right));
+            }
+            return minDepth + 1;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
