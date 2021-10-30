2304be078e8fc84a80e38da55e71684d35876120
hjforever <thjforever@hotmail.com>
2021-10-30 22:06:41 +0800

    leetcode question 21 solution
    链表

--- src/main/java/io/hjforever/leetcode/editor/cn/Q21MergeTwoSortedLists.java
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q21MergeTwoSortedLists.java
@@ -73,6 +73,7 @@ public class Q21MergeTwoSortedLists {
                 }
                 pre = pre.next ;
             }
+            // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
             pre.next = l1 == null ? l2 : l1;
             return result.next;
         }
