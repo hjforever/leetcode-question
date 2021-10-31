bdd413291933d5cabf6bd97a051a648a1e3aa219
hjforever <thjforever@hotmail.com>
2021-10-31 21:10:35 +0800

    leetcode question 83 solution
    链表

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q83RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,73 @@
+//存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+//
+// 返回同样按升序排列的结果链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序排列 
+// 
+// Related Topics 链表 👍 669 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+import io.hjforever.leetcode.editor.cn.common.ListNode;
+
+public class Q83RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new Q83RemoveDuplicatesFromSortedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+
+        //已排序，则相同的元素时连续的,只需要判定相邻直接是否相同即可
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode curr = head;
+            while (curr.next != null) {
+                if (curr.val == curr.next.val) {
+                    curr.next = curr.next.next;
+                } else {
+                    curr = curr.next;
+                }
+            }
+            return head;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
