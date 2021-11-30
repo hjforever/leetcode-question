6179a6c1770fcd102aa9470bb9130ef2dd390c0f
hjforever <thjforever@hotmail.com>
2021-11-30 18:34:42 +0800

    leetcode question 剑指offer 32 solution
    层级遍历

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/QJianZhiOffer32-IIICongShangDaoXiaDaYinErChaShuIII.java
@@ -0,0 +1,89 @@
+//请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回其层次遍历结果： 
+//
+// [
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+// Related Topics 树 广度优先搜索 二叉树 👍 153 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+import io.hjforever.leetcode.editor.cn.common.TreeNode;
+
+import java.util.*;
+ class QJianZhiOffer32IIICongShangDaoXiaDaYinErChaShuIII {
+    public static void main(String[] args) {
+        Solution solution = new QJianZhiOffer32IIICongShangDaoXiaDaYinErChaShuIII().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            boolean flag = false;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode treeNode = queue.poll();
+                    list.add(treeNode.val);
+                    if (treeNode.left != null) {
+                        queue.offer(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        queue.offer(treeNode.right);
+                    }
+                }
+                if (flag) {
+                    Collections.reverse(list);
+                    //System.out.println(Arrays.toString(list.toArray(new Integer[list.size()])));
+                }
+                flag = !flag;
+                res.add(list);
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
