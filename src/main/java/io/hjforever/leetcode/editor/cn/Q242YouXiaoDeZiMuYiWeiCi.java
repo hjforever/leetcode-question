473f2cdf2c48d3a16a5b8e9e8121f2117ea22f0e
Max <thjforever@hotmail.com>
2022-03-22 19:38:18 +0800

    leetcode question 242 solution
    map

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q242YouXiaoDeZiMuYiWeiCi.java
@@ -0,0 +1,69 @@
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//
+// 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "anagram", t = "nagaram"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "rat", t = "car"
+//输出: false 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length, t.length <= 5 * 10⁴ 
+// s 和 t 仅包含小写字母 
+// 
+//
+// 
+//
+// 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 哈希表 字符串 排序 👍 461 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+public class Q242YouXiaoDeZiMuYiWeiCi {
+    public static void main(String[] args) {
+        Solution solution = new Q242YouXiaoDeZiMuYiWeiCi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.equals(t)) {
+                return true;
+            }
+            if (s.length() != t.length()) {
+                return false;
+            }
+            int[] sCounts = new int[26];
+            int[] tCounts = new int[26];
+            for (char ch : s.toCharArray()) {
+                sCounts[ch - 'a']++;
+            }
+            for (char ch : t.toCharArray()) {
+                tCounts[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                if (sCounts[i] != tCounts[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
