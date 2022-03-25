98fdafca185c8fb20e818b660c870697765ed6f5
Max <thjforever@hotmail.com>
2022-03-25 09:50:59 +0800

    leetcode question 56 solution
    合并区间

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q56HeBingQuJian.java
@@ -0,0 +1,77 @@
+//以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返
+//回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+//输出：[[1,6],[8,10],[15,18]]
+//解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：intervals = [[1,4],[4,5]]
+//输出：[[1,5]]
+//解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= intervals.length <= 10⁴ 
+// intervals[i].length == 2 
+// 0 <= starti <= endi <= 10⁴ 
+// 
+// Related Topics 数组 排序 👍 1271 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Q56HeBingQuJian {
+    public static void main(String[] args) {
+        Solution solution = new Q56HeBingQuJian().new Solution();
+        solution.merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[][] merge(int[][] intervals) {
+            if (intervals == null || intervals.length == 0) return new int[0][0];
+            if (intervals.length == 1) return new int[][]{intervals[0]};
+            Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+            List<int[]> res = new ArrayList<>();
+            int start = intervals[0][0];
+            int end = intervals[0][1];
+            for (int i = 1; i < intervals.length; i++) {
+                int st = intervals[i][0];
+                int ed = intervals[i][1];
+                if (end < st) {
+                    int[] ans = new int[]{start, end};
+                    res.add(ans);
+                    start = st;
+                    end = ed;
+                } else {
+                    if (ed > end) {
+                        end = ed;
+                    }
+                }
+                if (i == intervals.length - 1) {
+                    res.add(new int[]{start, end});
+                }
+            }
+            return res.toArray(new int[res.size()][2]);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
