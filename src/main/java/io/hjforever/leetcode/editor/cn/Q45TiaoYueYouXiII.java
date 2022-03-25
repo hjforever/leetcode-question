fcd2904ab0afe9dd3ad43cc42750c449a8c0fc01
Max <thjforever@hotmail.com>
2022-03-25 10:19:10 +0800

    leetcode question 45 solution
    贪心

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q45TiaoYueYouXiII.java
@@ -0,0 +1,93 @@
+//给你一个非负整数数组 nums ，你最初位于数组的第一个位置。
+//
+// 数组中的每个元素代表你在该位置可以跳跃的最大长度。 
+//
+// 你的目标是使用最少的跳跃次数到达数组的最后一个位置。 
+//
+// 假设你总是可以到达数组的最后一个位置。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [2,3,1,1,4]
+//输出: 2
+//解释: 跳到最后一个位置的最小跳跃数是 2。
+//     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [2,3,0,1,4]
+//输出: 2
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// 0 <= nums[i] <= 1000 
+// 
+// Related Topics 贪心 数组 动态规划 👍 1332 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+public class Q45TiaoYueYouXiII {
+    public static void main(String[] args) {
+        Solution solution = new Q45TiaoYueYouXiII().new Solution();
+        System.out.println(solution.jump(new int[]{2, 3, 1, 1, 4}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int jump(int[] nums) {
+            // return jumpAfter(nums);
+            return jumpPre(nums);
+        }
+
+        //从后往前贪心，每次找到离终点最远的节点,这种算法比较直观好理解，最坏时间复杂度为O(n^2)
+        public int jumpAfter(int[] nums) {
+            if (nums == null || nums.length == 0) return -1;
+            if (nums.length == 1) {
+                return 0;
+            }
+            int len = nums.length;
+            int farIndex = len - 1;
+            int step = 0;
+            while (farIndex > 0) {
+                int k = farIndex;
+                for (int i = k - 1; i >= 0; i--) {
+                    if (nums[i] >= (k - i)) {
+                        farIndex = i;
+                    }
+                }
+                step++;
+            }
+            return step;
+
+        }
+
+        //从前往后贪心，每次找到能跳到的最远节点
+        public int jumpPre(int[] nums) {
+            int farPosition = 0;
+            int step = 0;
+            int end = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                farPosition = Math.max(farPosition, i + nums[i]);
+                if (i == end) {
+                    step++;
+                    end = farPosition;
+                }
+            }
+            return step;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
