3d709aaf20d1d610d951479a54bd2a4d35a9ff45
Max <thjforever@hotmail.com>
2022-06-10 10:35:13 +0800

    leetcode question 1244 solution
    map, priority queue

---
+++ src/main/java/io/hjforever/leetcode/editor/cn/Q1244DesignALeaderboard.java
@@ -0,0 +1,108 @@
+//Design a Leaderboard class, which has 3 functions:
+//
+// 
+// addScore(playerId, score): Update the leaderboard by adding score to the 
+//given player's score. If there is no player with such id in the leaderboard, add 
+//him to the leaderboard with the given score. 
+// top(K): Return the score sum of the top K players. 
+// reset(playerId): Reset the score of the player with the given id to 0 (in 
+//other words erase it from the leaderboard). It is guaranteed that the player was 
+//added to the leaderboard before calling this function. 
+// 
+//
+// Initially, the leaderboard is empty. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: 
+//["Leaderboard","addScore","addScore","addScore","addScore","addScore","top",
+//"reset","reset","addScore","top"]
+//[[],[1,73],[2,56],[3,39],[4,51],[5,4],[1],[1],[2],[2,51],[3]]
+//Output: 
+//[null,null,null,null,null,null,73,null,null,null,141]
+//
+//Explanation: 
+//Leaderboard leaderboard = new Leaderboard ();
+//leaderboard.addScore(1,73);   // leaderboard = [[1,73]];
+//leaderboard.addScore(2,56);   // leaderboard = [[1,73],[2,56]];
+//leaderboard.addScore(3,39);   // leaderboard = [[1,73],[2,56],[3,39]];
+//leaderboard.addScore(4,51);   // leaderboard = [[1,73],[2,56],[3,39],[4,51]];
+//leaderboard.addScore(5,4);    // leaderboard = [[1,73],[2,56],[3,39],[4,51],[5
+//,4]];
+//leaderboard.top(1);           // returns 73;
+//leaderboard.reset(1);         // leaderboard = [[2,56],[3,39],[4,51],[5,4]];
+//leaderboard.reset(2);         // leaderboard = [[3,39],[4,51],[5,4]];
+//leaderboard.addScore(2,51);   // leaderboard = [[2,51],[3,39],[4,51],[5,4]];
+//leaderboard.top(3);           // returns 141 = 51 + 51 + 39;
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= playerId, K <= 10000 
+// It's guaranteed that K is less than or equal to the current number of 
+//players. 
+// 1 <= score <= 100 
+// There will be at most 1000 function calls. 
+// 
+// Related Topics 设计 哈希表 排序 👍 33 👎 0
+
+
+package io.hjforever.leetcode.editor.cn;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Q1244DesignALeaderboard {
+    public static void main(String[] args) {
+        //Solution solution = new Q1244DesignALeaderboard().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Leaderboard {
+
+
+        Map<Integer, Integer> map;
+
+
+        public Leaderboard() {
+            map = new HashMap<>();
+        }
+
+        public void addScore(int playerId, int score) {
+            map.put(playerId, map.getOrDefault(playerId, 0) + score);
+        }
+
+        public int top(int K) {
+            //大根堆
+            PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+            for (Integer value : map.values()) {
+                queue.offer(value);
+            }
+            int sum = 0;
+            for (int i = 0; i < K; i++) {
+                sum += queue.poll();
+            }
+            return sum;
+        }
+
+        public void reset(int playerId) {
+            map.remove(playerId);
+        }
+    }
+
+/**
+ * Your Leaderboard object will be instantiated and called as such:
+ * Leaderboard obj = new Leaderboard();
+ * obj.addScore(playerId,score);
+ * int param_2 = obj.top(K);
+ * obj.reset(playerId);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
